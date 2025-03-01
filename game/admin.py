c933b85a137ece4ba3d2062578db46cec3724cfe
Michał Rokita <mrokita@mrokita.pl>
2025-03-01 11:57:56 +0100

    Initial commit

---
+++ game/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from game.models import Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    ...
---
+++ game/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+class Task(models.Model):
+    description: str = models.TextField(
+        verbose_name=_("Task description")
+    )
